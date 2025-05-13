c3c0bb84935c2f560804a839a6a4860a0e8973ae
oktaydemirhas <oktaydemirhas@gmail.com>
2025-05-13 16:53:00 +0300

    Bedroom sınıfı tamamlandı.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,17 @@
 package org.example;
 
+import org.example.model.Bed;
+import org.example.model.enums.LampType;
+import org.example.model.enums.PaintColor;
+import org.example.model.Lamp;
+import org.example.model.Wall;
+import org.example.model.Ceiling;
+import org.example.model.Carpet;
+import org.example.model.Wardrobe;
+import org.example.model.Bedroom;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        
     }
 }
